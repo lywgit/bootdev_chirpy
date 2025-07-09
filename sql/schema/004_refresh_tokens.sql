d110d7404d2c050d15d4942ecd98a78d4e3d79f2
lywang <wellmax111@gmail.com>
2025-07-09 17:10:20 +0800

    [fix] use timestamptz instead of timestamp

--- sql/schema/004_refresh_tokens.sql
+++ sql/schema/004_refresh_tokens.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE refresh_tokens (
     token TEXT PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
     user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    expires_at TIMESTAMP NOT NULL,
-    revoked_at TIMESTAMP
+    expires_at TIMESTAMPTZ NOT NULL,
+    revoked_at TIMESTAMPTZ
 );
 
 -- +goose Down
